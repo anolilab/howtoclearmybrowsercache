e056202cc20e693c0fe39c8d70d74fe60ab76a82
daniel bannert <d.bannert@anolilab.de>
2016-02-13 17:28:40 +0100

    small change on grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,7 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
+    pkg: grunt.file.readJSON(‘package.json’),
     webfont: {
       icons: {
         src: 'icons/*.svg',
